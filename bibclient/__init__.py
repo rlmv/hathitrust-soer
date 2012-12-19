c1cfabea5d96ef408cc476952dcd3207f523e6f2
robertmarchman <robert.l.marchman@dartmouth.edu>
2012-12-20 12:16:13 +1300

    revised json request methods

--- bibclient/__init__.py
+++ bibclient/__init__.py
@@ -2,4 +2,14 @@
 
 __author__ = 'robertmarchman'
 
+BIB_BASEURL = 'http://catalog.hathitrust.org/api/volumes/'
 
+ID_TYPES = ['oclc',
+            'lccn',
+            'issn',
+            'isbn',
+            'htid',
+            'recordnumber',
+            ]
+
+REQUEST_TYPES = ['json']
