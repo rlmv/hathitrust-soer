ce2c753b71e494760aaf11360766c5f15404f735
robertmarchman <robert.l.marchman@dartmouth.edu>
2012-12-21 15:38:15 +1300

    added constants.py module

--- bibclient/__init__.py
+++ bibclient/__init__.py
@@ -1,15 +1,2 @@
 
-
 __author__ = 'robertmarchman'
-
-BIB_BASEURL = 'http://catalog.hathitrust.org/api/volumes/'
-
-ID_TYPES = ['oclc',
-            'lccn',
-            'issn',
-            'isbn',
-            'htid',
-            'recordnumber',
-            ]
-
-REQUEST_TYPES = ['json']
