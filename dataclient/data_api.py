a18f3b699174ab24c89cf42db67e5e99efcb76cd
robertmarchman <robert.l.marchman@dartmouth.edu>
2013-01-03 23:07:12 +1300

    basic data get methods implemented

--- dataclient/data_api.py
+++ dataclient/data_api.py
@@ -4,6 +4,8 @@ __author__ = 'robertmarchman'
 
 import requests
 import json
+import xml.etree.ElementTree as ET
+
 
 from requests_oauthlib import OAuth1
 from constants import DATA_BASEURL, SECURE_DATA_BASEURL
@@ -12,12 +14,14 @@ from constants import DATA_BASEURL, SECURE_DATA_BASEURL
 class HTDataInterface(object):
 
     def __init__(self, client_key, client_secret, secure=False):
-        """Initialize a HTDataInterface object.
+        """
+        Initialize a HTDataInterface object.
 
-        :param client_key: OAuth client client key
-        :param client_secret: secret OAuth key
-        :param secure: toggles http/https session. Defaults to
-        http, use https for access to restricted content.
+        Args:
+            client_key: OAuth client client key
+            client_secret: secret OAuth key
+            secure: toggles http/https session. Defaults to
+                 http, use https for access to restricted content.
 
         """
 
@@ -38,21 +42,88 @@ class HTDataInterface(object):
             self.baseurl = DATA_BASEURL
 
 
-    def make_request(self, url):
-        """Process generic URI request."""
+    def _make_request(self, resource, doc_id, sequence=None, 
+                        v=1, json=False, callback=None):
+        """
+        Construct and perform URI request.
 
-        return self.rsession.get(url)
+        Args:
+            resource: resource type
+            doc_id: document identifier of target
+            sequence: page number for single page resources
+            v: API version 
+            json: if json=True, the json representation of
+                the resource is returned. Only valid for resources that 
+                are xml or xml+atom by default.
+            callback: optional javascript callback function, 
+                which only has an effect if json=True.
+
+        Returns: 
+            requests response object
+
+        Note there's not much error checking on url construction, 
+        but errors do get raised after badly formed requests.
+
+        """
+
+        url = "".join([self.baseurl, resource, '/', doc_id])
         
+        if sequence:
+            url += '/' + str(sequence)
+
+        # construct optional parameter dictionary
+        params = {'v': str(v)}
+        if json:
+            params['alt'] = 'json'
+            if callback:
+                params['callback'] = callback
+
+        # perform request and raise errors
+        r = self.rsession.get(url, params=params)
+        r.raise_for_status()
+
+        return r
 
 
-    def get_meta(self):
-        pass
+    def get_meta(self, doc_id, json=False):
+        """
+        Retrieve Volume and Rights Metadata resources.
 
-    def get_structure(self):
-        pass
+        Args:
+            doc_id: document identifier
+            json: if json=True, the json representation of
+                the resource is returned, otherwise efaults to an atom+xml 
+                format.
 
-    def get_aggregate(self):
-        pass
+        Returns: 
+            ...
+
+        """
+        return self._make_request('meta', doc_id, json=json)
+
+
+    def get_structure(self, doc_id, json=False):
+        """ 
+        Retrieve a METS document.
+
+        Args:
+            doc_id: target document
+            json: toggles json/xml 
+        Returns:
+            xml or json string
+
+        """
+        return self._make_request('structure', doc_id, json=json)
+
+
+    def get_pagemeta(self, doc_id, seq, json=False):
+        """ Retrieve single page metadata. """
+
+        return self._make_request('pagemeta', doc_id, sequence=seq, json=json)
+
+
+    def get_aggregate(self, doc_id):
+        return self._make_request('aggregate', doc_id)
 
     def get_pageimage(self):
         pass
@@ -67,7 +138,7 @@ class HTDataInterface(object):
 
 if __name__ == "__main__":
 
-    url = u'https://babel.hathitrust.org/cgi/htd/meta/miun.abr0732.0001.001'
+    url = u'https://babel.hathitrust.org/cgi/htd/meta/miun.abr0732.0001.001/2?v=1'
 
     client_key = u'd4b43a0b2e'
     client_secret= u'f53b0161addebb83d1baf03f7b69'
@@ -78,8 +149,18 @@ if __name__ == "__main__":
     # print r.text
 
     diface = HTDataInterface(client_key, client_secret, secure=False)
-    print diface.make_request(url).text
-    print diface.make_request(url).url
+    # js = diface.make_request(url).json()
+    # print json.dumps(js, indent=4)
+    # response = diface._make_request('meta', 'miun.abr0732.0001.001', v=1, json=True)
+    response = diface.get_aggregate('miun.abr0732.0001.001')
+
+    with open('temp.zip', 'wb') as f:
+        f.write(response.content)
+
+    print 'DONE'
+
+  
+   
 
 
    
