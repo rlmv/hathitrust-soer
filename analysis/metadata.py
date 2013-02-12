c5e77f26a45fd71234e1c8344228109b316db05d
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-13 12:46:39 +1300

    publication date mapping function

--- analysis/metadata.py
+++ analysis/metadata.py
@@ -2,11 +2,12 @@
 import glob
 import os
 import re
+from collections import Counter
 
 from pymarc import parse_xml_to_array
 
 
-def retrieve_xml_paths(target_dir):
+def retrieve_xml_paths_from_dir(target_dir):
     """ Generator over absolute filepaths for xml metadata files.
 
     Args:
@@ -17,6 +18,10 @@ def retrieve_xml_paths(target_dir):
 
     for x in l:
         yield os.path.abspath(x) 
+
+
+def retrieve_xml_paths_from_pairtree(doc_ids):
+    pass
 
 
 def get_record_from_METS(fpath):
@@ -55,17 +60,37 @@ def normalize_year(year_string):
     return max(map(int, matches)) if matches else None
 
   
+def map_publication_years(records):
+    """ Map the publication years of a collection into a dictionary. 
+
+    This will produce year:number pairs, which relates the number of
+    documents published in a given year to that year. Years are integers.
+
+    Args: 
+    -collection :  an iterator over pymarc Record objects.
+    """
+
+    mapping = Counter()
+
+    for r in records:
+        year = r.pubyear()
+        year = normalize_year(year)
+
+        mapping[year] += 1
+
+    return mapping
+
+
 
 if __name__ == "__main__":
 
     v = '../htrc_api/m'
     x = './data/raw'  
+
+    paths = retrieve_xml_paths_from_dir(v)
+    records = [get_record_from_METS(p) for p in paths]
+    # for r in records:
+    #     print normalize_year(r.pubyear())
     
-
-    for p in retrieve_xml_paths(v):
-        r = get_record_from_METS(p)
-        year = r.pubyear()
-
-        print normalize_year(year)
-    print(normalize_year(None))
+    d = map_publication_years(records)
 
