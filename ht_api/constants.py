b9610170f676b47ad2bf0327dc634cc4d50b71b0
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-15 13:00:04 +1300

    rearrange directory structure

--- ht_api/constants.py
+++ ht_api/constants.py
@@ -2,9 +2,23 @@
 
 __author__ = 'robertmarchman'
 
+
+# constants for bibliographic api
+
+BIB_BASEURL = 'http://catalog.hathitrust.org/api/volumes/'
+ID_TYPES = ['oclc',
+            'lccn',
+            'issn',
+            'isbn',
+            'htid',
+            'recordnumber',
+            ]
+
+
+# constants for data api
+
 DATA_BASEURL = 'http://babel.hathitrust.org/cgi/htd/'
 SECURE_DATA_BASEURL = 'https://babel.hathitrust.org/cgi/htd/'
-
 RESOURCES = ['meta',
             'structure',
             'aggregate',
@@ -13,3 +27,5 @@ RESOURCES = ['meta',
             'pageocr',
             'pagecoordocr',
             ]
+
+
