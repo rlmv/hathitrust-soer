1022e110147e074c16c9265dfd6480c4a0f86da5
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-27 12:36:44 +1300

    refactor and optimize

--- identify.py
+++ identify.py
@@ -5,7 +5,20 @@ from string import lower
 from marc import MarcSQLite, get_id_from_record
 
 
+def has_reference(record):
+    for field in record.subjects():
+        try:
+            subj = lower(field.get_subfields('a')[0])   
+            for term in terms:
+                if term in subj:
+                    return True
+        except IndexError:
+            pass
+    return False
+
+
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('MARCDB',
                         help='MarcSQLite database from which to retrieve records.')
@@ -16,7 +29,6 @@ if __name__ == "__main__":
                         help='Optional file to write output to.')
 
     args = parser.parse_args()
-    print args
 
     terms = [lower(s) for s in args.TERM]
 
@@ -26,16 +38,7 @@ if __name__ == "__main__":
 
         with m:
             for r in m.get_all_records():
-                matches = []
-                for field in r.subjects():
-                    try:
-                        subj = lower(field.get_subfields('a')[0])   
-                        for term in terms:
-                            if term in subj:
-                                matches.append(subj)
-                    except IndexError:
-                        pass
-                if matches:
+                if has_reference(r):
                     id_ = get_id_from_record(r)
                     print id_
                     f_id.write("{}\n".format(id_))
