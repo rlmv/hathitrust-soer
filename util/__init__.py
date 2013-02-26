5709ac36d3f11259f7abb2881d0138bab1069c0c
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-26 15:27:28 +1300

    comment

--- util/__init__.py
+++ util/__init__.py
@@ -7,4 +7,5 @@ from .file_id_iter import file_id_iter
 from .py3 import require_py3, is_py3
 
 if not is_py3():
+    # Python3 is unicode compatible by default
     from .unicodecsvwriter import UnicodeWriter
