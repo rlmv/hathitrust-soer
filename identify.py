c546b5681759ef89da045aaa43da8e52d8d2583f
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-27 12:25:49 +1300

    converted to command line app

--- identify.py
+++ identify.py
@@ -1,24 +1,41 @@
 
-import re
+import argparse
+from string import lower
 
 from marc import MarcSQLite, get_id_from_record
 
 
-REGEX = re.compile(r'new zealand|maori', re.IGNORECASE)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('MARCDB',
+                        help='MarcSQLite database from which to retrieve records.')
+    parser.add_argument('TERM',
+                        nargs='+',
+                        help='Terms signalling positive identification.')
+    parser.add_argument('--out', 
+                        help='Optional file to write output to.')
 
-m = MarcSQLite('results/non_google.20111101_01.db')
+    args = parser.parse_args()
+    print args
 
-with open('results/non_google_nz.txt', 'w') as f_id:
+    terms = [lower(s) for s in args.TERM]
 
-    with m:
-        for r in m.get_all_records():
-            matches = []
-            for field in r.subjects():
-                try:
-                    subj = field.get_subfields('a')[0]
-                    found = REGEX.findall(subj)
-                    matches += found
-                except IndexError:
-                    pass
-            if matches:
-                f_id.write("{}\n".format(get_id_from_record(r)))
+    m = MarcSQLite(args.MARCDB)
+
+    with open(args.out, 'w') as f_id:
+
+        with m:
+            for r in m.get_all_records():
+                matches = []
+                for field in r.subjects():
+                    try:
+                        subj = lower(field.get_subfields('a')[0])   
+                        for term in terms:
+                            if term in subj:
+                                matches.append(subj)
+                    except IndexError:
+                        pass
+                if matches:
+                    id_ = get_id_from_record(r)
+                    print id_
+                    f_id.write("{}\n".format(id_))
