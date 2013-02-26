47b5359e3bb575d1bdf28bfe3dc53b8ab9c41c71
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-26 13:49:02 +1300

    make db args explicit

--- build_db_from_xml.py
+++ build_db_from_xml.py
@@ -24,18 +24,13 @@ if __name__ == "__main__":
     parser.add_argument("SOURCE_XML", 
         help="A multi-record MarcXML file.")
     parser.add_argument("TARGET_DB", 
-        help="Name of database to create.", 
-        nargs="?", 
-        default=None)
+        help="Name of database to create.")
 
     args = parser.parse_args()
 
     source = args.SOURCE_XML
-    if args.TARGET_DB:
-        target = args.TARGET_DB
-    else:
-        target = source.rsplit(".xml", 1)[0] + ".db"
-
+    target = args.TARGET_DB
+    
     parse_xml_to_SQLite(source, target)
 
 
